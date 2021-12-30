13c25dc2c50fb742598e370676ef17ba878db610
Jenny Li <jing.li@zilliz.com>
2021-12-31 01:15:11 +0800

    Fix python install conflict
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- build/ci/jenkins/PR.groovy
+++ build/ci/jenkins/PR.groovy
@@ -137,6 +137,15 @@ pipeline {
                         }
                     }
                     stage('E2E Test'){
+                        agent {
+                            kubernetes {
+                                label 'milvus-e2e-pytest'
+                                inheritFrom 'default'
+                                defaultContainer 'main'
+                                yamlFile 'build/ci/jenkins/pod/rte.yaml'
+                                customWorkspace '/home/jenkins/agent/workspace'
+                            }
+                        }
                         steps {
                             container('pytest') {
                                 dir ('tests/scripts') {
@@ -156,6 +165,12 @@ pipeline {
                                         } else {
                                         error "Error: Unsupported Milvus client: ${MILVUS_CLIENT}"
                                         }
+                                         dir("${env.ARTIFACTS}") {
+                                            if ("${MILVUS_CLIENT}" == "pymilvus") {
+                                                sh "tar -zcvf artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${MILVUS_CLIENT}-pytest-logs.tar.gz /tmp/ci_logs/test --remove-files || true"
+                                                }
+                                            archiveArtifacts artifacts: "artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${MILVUS_CLIENT}-pytest-logs.tar.gz ", allowEmptyArchive: true
+                                        }
                                     }
                                 }
                             }
@@ -170,22 +185,11 @@ pipeline {
                                 script {
                                     def release_name=sh(returnStdout: true, script: './get_release_name.sh')
                                     sh "./uninstall_milvus.sh --release-name ${release_name}"
-                                }
-                            }
-                        }
-                        container('pytest') {
-                            dir ('tests/scripts') {
-                                script {
-                                        def release_name = sh(returnStdout: true, script: './get_release_name.sh ')
-                                        sh "./ci_logs.sh --log-dir /ci-logs  --artifacts-name ${env.ARTIFACTS}/artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${SEMVER}-${env.BUILD_NUMBER}-${MILVUS_CLIENT}-e2e-logs \
+                                    sh "./ci_logs.sh --log-dir /ci-logs  --artifacts-name ${env.ARTIFACTS}/artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${SEMVER}-${env.BUILD_NUMBER}-${MILVUS_CLIENT}-e2e-logs \
                                         --release-name ${release_name}"
-                                        dir("${env.ARTIFACTS}") {
-                                            if ("${MILVUS_CLIENT}" == "pymilvus") {
-                                                sh "tar -zcvf artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${MILVUS_CLIENT}-pytest-logs.tar.gz /tmp/ci_logs/test --remove-files || true"
-                                                }
-                                            archiveArtifacts artifacts: "artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${MILVUS_CLIENT}-pytest-logs.tar.gz ", allowEmptyArchive: true
-                                            archiveArtifacts artifacts: "artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${SEMVER}-${env.BUILD_NUMBER}-${MILVUS_CLIENT}-e2e-logs.tar.gz", allowEmptyArchive: true
-                                        }
+                                    dir("${env.ARTIFACTS}") {
+                                        archiveArtifacts artifacts: "artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${SEMVER}-${env.BUILD_NUMBER}-${MILVUS_CLIENT}-e2e-logs.tar.gz", allowEmptyArchive: true
+                                    }
                                 }
                             }
                         }
