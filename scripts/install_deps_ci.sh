25516e9d5ce1c87187cd373523c19593f95659f3
Jenny Li <jing.li@zilliz.com>
2022-01-26 17:42:26 +0800

    Update deps for code checker
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

---
+++ scripts/install_deps_ci.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Licensed to the LF AI & Data foundation under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function install_mac_deps() {
+  brew install boost libomp ninja tbb
+}
+
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Darwin*)    install_mac_deps;;
+    *)          echo "Unsupported OS:${unameOut}" ; exit 0;
+esac
