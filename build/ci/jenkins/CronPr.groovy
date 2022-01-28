ad8ec53477ec31ce4a38e3174c52fc3cde4bb0a8
Jenny Li <jing.li@zilliz.com>
2022-01-28 11:48:39 +0800

    update time
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- build/ci/jenkins/CronPr.groovy
+++ build/ci/jenkins/CronPr.groovy
@@ -5,7 +5,7 @@ int e2e_timeout_seconds = 120 * 60
 // def imageTag=''
 int case_timeout_seconds = 10 * 60
 String cron_timezone = 'TZ=Asia/Shanghai'
-String cron_string =  "H 10 * * * " 
+String cron_string =  "H 11 * * * " 
 def chart_version='2.5.0'
 pipeline {
     triggers {
