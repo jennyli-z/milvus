261fd38eb0c6ab40169dd00f10f54923ac22bd21
Jenny Li <jing.li@zilliz.com>
2022-03-28 22:26:19 +0800

    Update chart to 3.0.14
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- build/ci/jenkins/kafka.groovy
+++ build/ci/jenkins/kafka.groovy
@@ -4,7 +4,7 @@ int total_timeout_minutes = 120
 int e2e_timeout_seconds = 70 * 60
 def imageTag='PR-16149-20220328-2c425f781'
 int case_timeout_seconds = 10 * 60
-def chart_version='3.0.5'
+def chart_version='3.0.14'
 pipeline {
     options {
         timestamps()
